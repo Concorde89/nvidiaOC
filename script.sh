71f6729a6b079019f1ffecda1d21d9b22268132e
Concorde89 <33375316+Concorde89@users.noreply.github.com>
2018-01-20 22:54:04 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,41 @@
+nvidia-settings -a '[gpu:0]/GPUFanControlState=1'
+
+nvidia-settings -a '[fan:0]/GPUTargetFanSpeed=100'
+
+nvidia-settings -a '[gpu:1]/GPUFanControlState=1'
+
+nvidia-settings -a '[fan:1]/GPUTargetFanSpeed=100'
+
+nvidia-settings -a '[gpu:2]/GPUFanControlState=1'
+
+nvidia-settings -a '[fan:2]/GPUTargetFanSpeed=100'
+
+nvidia-settings -a '[gpu:3]/GPUFanControlState=1'
+
+nvidia-settings -a '[fan:3]/GPUTargetFanSpeed=100'
+
+nvidia-settings -a '[gpu:4]/GPUFanControlState=1'
+
+nvidia-settings -a '[fan:4]/GPUTargetFanSpeed=100'
+
+
+nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=1430'
+
+nvidia-settings -a '[gpu:1]/GPUMemoryTransferRateOffset[3]=1430'
+
+nvidia-settings -a '[gpu:2]/GPUMemoryTransferRateOffset[3]=1430'
+
+nvidia-settings -a '[gpu:3]/GPUMemoryTransferRateOffset[3]=1430'
+
+nvidia-settings -a '[gpu:4]/GPUMemoryTransferRateOffset[3]=1430'
+
+
+nvidia-settings -a '[GPU:0]/GPUGraphicsClockOffset[3]=+110'
+
+nvidia-settings -a '[GPU:1]/GPUGraphicsClockOffset[3]=+110'
+
+nvidia-settings -a '[GPU:2]/GPUGraphicsClockOffset[3]=+110'
+
+nvidia-settings -a '[GPU:3]/GPUGraphicsClockOffset[3]=+110'
+
+nvidia-settings -a '[GPU:4]/GPUGraphicsClockOffset[3]=+110'
